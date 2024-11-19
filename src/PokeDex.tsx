0ae77b257533964401d91917da27a97b4f9f8250
Tonipenyallop <kimtesu521@gmail.com>
2024-11-19 23:22:09 +0900

    get: first gen pokemon names

--- src/PokeDex.tsx
+++ src/PokeDex.tsx
@@ -1,23 +1,41 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import "./PokeDex.css";
 import { PokedexService } from "./services/pokedex-service";
 
-function PokeDex() {
-  const [count, setCount] = useState(0);
-  const pokemons = Array.from({ length: 5 }, (_val, idx) => ({
-    name: `pokemon${idx}`,
-  }));
+const PokeDex = () => {
+  const [pokemons, setPokemons] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
   const pokemonService = PokedexService.getInstance();
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      try {
+        const res = await pokemonService.getPokemons();
+        setPokemons(res);
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchPokemons();
+  }, []);
+
+  if (isLoading) {
+    return;
+  }
 
   return (
     <div className="pokedex-container">
-      {pokemons.map((pokemon) => (
-        <div className="">{pokemon.name}</div>
+      {pokemons.map((pokemon: { name: string; url: string }) => (
+        <div className="" key={pokemon.name}>
+          {pokemon.name}
+        </div>
       ))}
-      <button onClick={pokemonService.getPokemons}>CLICK</button>
+      <button onClick={() => console.log("clicked")}>CLICK</button>
     </div>
   );
-}
+};
 
 export default PokeDex;
--- src/services/pokedex-service.ts
+++ src/services/pokedex-service.ts
@@ -12,13 +12,27 @@ export class PokedexService {
     return PokedexService.instance;
   }
 
-  getPokemons() {
+  async getPokemons() {
     console.log("getPokemons was called");
-    // const PORT = "8080";
-    // axios.get(`http://localhost:8080`);
-
-    // const res = (await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=151`))
-    //   .data.results;
-    // console.log("res", res);
+    try {
+      const res = await axios.get(`http://localhost:8080/pokemon`);
+      if (!res) {
+        throw new Error("failed to fetch pokemon");
+      }
+      console.log("res.data", res.data.results);
+      return res.data.results;
+    } catch (err) {
+      if (axios.isAxiosError(err)) {
+        throw new Error(
+          `Failed to fetch pokemons: ${
+            err.response
+              ? `Status ${err.response.status} - ${err.response.statusText}`
+              : err.message
+          }`
+        );
+      } else {
+        throw new Error(`unknown error happened: ${err}`);
+      }
+    }
   }
 }
