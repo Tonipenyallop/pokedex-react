26fe482ced5db91a714a87d0e27ed78c5d6395dd
Tonipenyallop <kimtesu521@gmail.com>
2024-11-21 23:08:30 +0900

    add: musics

---
+++ src/musics.ts
@@ -0,0 +1,93 @@
+import sound1 from "../../PokemonRG_Music/01 ～オープニング～.wav";
+import sound2 from "../../PokemonRG_Music/02 マサラタウンのテーマ.wav";
+import sound3 from "../../PokemonRG_Music/03 オーキド博士.wav";
+import sound4 from "../../PokemonRG_Music/04 オーキド研究所.wav";
+import sound5 from "../../PokemonRG_Music/05 ライバルあらわる.wav";
+import sound6 from "../../PokemonRG_Music/06 トキワへの道－マサラより.wav";
+import sound7 from "../../PokemonRG_Music/07 戦い（ＶＳ野生ポケモン）.wav";
+import sound8 from "../../PokemonRG_Music/08 勝利（ＶＳ野生ポケモン）.wav";
+import sound9 from "../../PokemonRG_Music/09 ニビシティのテーマ.wav";
+import sound10 from "../../PokemonRG_Music/10 ポケモンセンター.wav";
+import sound11 from "../../PokemonRG_Music/11 ポケモン回復.wav";
+import sound12 from "../../PokemonRG_Music/12 トキワの森.wav";
+import sound13 from "../../PokemonRG_Music/13 道案内.wav";
+import sound14 from "../../PokemonRG_Music/14 トレーナーあらわる（女の子編）.wav";
+import sound15 from "../../PokemonRG_Music/15 戦い（ＶＳトレーナー）.wav";
+import sound16 from "../../PokemonRG_Music/16 勝利（ＶＳトレーナー）..wav";
+import sound17 from "../../PokemonRG_Music/17 おつきみ山のどうくつ.wav";
+import sound18 from "../../PokemonRG_Music/18 ハナダへの道－おつきみ山より.wav";
+import sound19 from "../../PokemonRG_Music/19 ハナダシティのテーマ.wav";
+import sound20 from "../../PokemonRG_Music/20 ポケモンジム.wav";
+import sound21 from "../../PokemonRG_Music/21 マサキのもとへ－ハナダより.wav";
+import sound22 from "../../PokemonRG_Music/22 プリンの歌.wav";
+import sound23 from "../../PokemonRG_Music/23 クチバシティのテーマ.wav";
+import sound24 from "../../PokemonRG_Music/24 サントアンヌ号.wav";
+import sound25 from "../../PokemonRG_Music/25 シオンへの道－クチバより.wav";
+import sound26 from "../../PokemonRG_Music/26 ポケモンの笛.wav";
+import sound27 from "../../PokemonRG_Music/27 トレーナーあらわる（男の子編）.wav";
+import sound28 from "../../PokemonRG_Music/28 戦い（ＶＳジムリーダー）.wav";
+import sound29 from "../../PokemonRG_Music/29 勝利（ＶＳジムリーダー）.wav";
+import sound30 from "../../PokemonRG_Music/30 サイクリング.wav";
+import sound31 from "../../PokemonRG_Music/31 シオンタウンのテーマ.wav";
+import sound32 from "../../PokemonRG_Music/32 ポケモンタワー.wav";
+import sound33 from "../../PokemonRG_Music/33 タマムシシティのテーマ.wav";
+import sound34 from "../../PokemonRG_Music/34 カジノ.wav";
+import sound35 from "../../PokemonRG_Music/35 トレーナーあらわる（悪いヤツ編）.wav";
+import sound36 from "../../PokemonRG_Music/36 ロケット団アジト.wav";
+import sound37 from "../../PokemonRG_Music/37 シルフカンパニー.wav";
+import sound38 from "../../PokemonRG_Music/38 海.wav";
+import sound39 from "../../PokemonRG_Music/39 グレンタウンのテーマ.wav";
+import sound40 from "../../PokemonRG_Music/40 ポケモン屋敷.wav";
+import sound41 from "../../PokemonRG_Music/41 進化.wav";
+import sound42 from "../../PokemonRG_Music/42 最後の道.wav";
+import sound43 from "../../PokemonRG_Music/43 ラストバトル（ＶＳライバル）.wav";
+import sound44 from "../../PokemonRG_Music/44 殿堂入り.wav";
+import sound45 from "../../PokemonRG_Music/45 ～エンディング～.wav";
+
+export const musics = [
+  sound1,
+  sound2,
+  sound3,
+  sound4,
+  sound5,
+  sound6,
+  sound7,
+  sound8,
+  sound9,
+  sound10,
+  sound11,
+  sound12,
+  sound13,
+  sound14,
+  sound15,
+  sound16,
+  sound17,
+  sound18,
+  sound19,
+  sound20,
+  sound21,
+  sound22,
+  sound23,
+  sound24,
+  sound25,
+  sound26,
+  sound27,
+  sound28,
+  sound29,
+  sound30,
+  sound31,
+  sound32,
+  sound33,
+  sound34,
+  sound35,
+  sound36,
+  sound37,
+  sound38,
+  sound39,
+  sound40,
+  sound41,
+  sound42,
+  sound43,
+  sound44,
+  sound45,
+];
