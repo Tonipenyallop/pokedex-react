59345f65e8c47d9d197904bb667cda7bd43b5693
Tonipenyallop <kimtesu521@gmail.com>
2024-11-21 23:09:52 +0900

    add: pokemon detrail, music

--- src/PokeDex.tsx
+++ src/PokeDex.tsx
@@ -1,18 +1,50 @@
-import { useState, useEffect } from "react";
-
+import { useState, useEffect, useRef } from "react";
 import "./PokeDex.css";
 import { PokedexService } from "./services/pokedex-service";
+import { Pokemon } from "./type";
+import { musics } from "./musics";
+
+const BGM_MAX_NUM_IDX = 45;
 
 const PokeDex = () => {
   const [pokemons, setPokemons] = useState();
   const [isLoading, setIsLoading] = useState(true);
-
+  // 649 -> till BW
+  const pokemonIds = Array.from({ length: 151 }, (_, idx) => `${idx + 1}`);
   const pokemonService = PokedexService.getInstance();
+
+  // pallet town music
+  const [musicIndex, setMusicIndex] = useState(1);
+
+  function playNextMusic() {
+    const newIdx = (musicIndex + 1) % BGM_MAX_NUM_IDX;
+    setMusicIndex(newIdx);
+    setTimeout(() => {
+      if (audioRef.current === null) {
+        return;
+      }
+      audioRef.current.play();
+    }, 1000);
+  }
+
+  function playPrevMusic() {
+    const newIdx = musicIndex === 0 ? BGM_MAX_NUM_IDX - 1 : musicIndex - 1;
+    setMusicIndex(newIdx);
+    setTimeout(() => {
+      if (audioRef.current === null) {
+        return;
+      }
+      audioRef.current.play();
+    }, 1000);
+  }
+
   useEffect(() => {
     const fetchPokemons = async () => {
       try {
-        const res = await pokemonService.getPokemons();
-        setPokemons(res);
+        const pokemons = await pokemonService.getMultiplePokemonDetails(
+          pokemonIds
+        );
+        setPokemons(pokemons);
       } catch (err) {
         console.error(err);
       } finally {
@@ -22,18 +54,52 @@ const PokeDex = () => {
     fetchPokemons();
   }, []);
 
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  const handleEnded = () => {
+    console.log("handleEnded was called");
+    playNextMusic(); // Automatically play the next song
+  };
+
+  const handlePlayNext = () => {
+    playNextMusic();
+    if (audioRef.current) {
+      audioRef.current.play(); // Play the next song
+    }
+  };
+
   if (isLoading) {
     return;
   }
 
   return (
     <div className="pokedex-container">
-      {pokemons.map((pokemon: { name: string; url: string }) => (
-        <div className="" key={pokemon.name}>
-          {pokemon.name}
-        </div>
-      ))}
-      <button onClick={() => console.log("clicked")}>CLICK</button>
+      <button onClick={playPrevMusic}>PREV MUSIC</button>
+      <audio
+        ref={audioRef}
+        src={musics[musicIndex]}
+        controls
+        onEnded={handleEnded}
+      />
+      <button onClick={handlePlayNext}>NEXT MUSIC</button>
+
+      <div className="pokemon-card-container">
+        {pokemons.map((pokemon: Pokemon) => (
+          <div className="pokemon-card" key={pokemon.name}>
+            <div className="pokemon-info-container">
+              <div className="">{pokemon.name}</div>
+              <div className="type-container">
+                <div className="type">{pokemon.types[0].type.name}</div>
+                <div className="type">
+                  {pokemon.types[1] && pokemon.types[1].type.name}
+                </div>
+              </div>
+            </div>
+
+            <img src={pokemon.sprites.front_default || ""} alt="" />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
