b8d132222e52eb30fb11c55d16651bc9678bff00
Tonipenyallop <kimtesu521@gmail.com>
2024-11-21 23:09:17 +0900

    add: type

---
+++ src/type.ts
@@ -0,0 +1,76 @@
+export type Pokemon = {
+  abilities: {
+    ability: {
+      name: string;
+      url: string;
+    };
+    is_hidden: boolean;
+    slot: number;
+  }[];
+  base_experience: number;
+  forms: {
+    name: string;
+    url: string;
+  }[];
+  game_indices: {
+    game_index: number;
+    version: {
+      name: string;
+      url: string;
+    };
+  }[];
+  height: number;
+  held_items: any[]; // Replace `any` with a specific type if you know the structure of held items
+  id: number;
+  is_default: boolean;
+  location_area_encounters: string;
+  moves: {
+    move: {
+      name: string;
+      url: string;
+    };
+    version_group_details: {
+      level_learned_at: number;
+      move_learn_method: {
+        name: string;
+        url: string;
+      };
+      version_group: {
+        name: string;
+        url: string;
+      };
+    }[];
+  }[];
+  name: string;
+  order: number;
+  species: {
+    name: string;
+    url: string;
+  };
+  sprites: {
+    back_default: string | null;
+    back_female: string | null;
+    back_shiny: string | null;
+    back_shiny_female: string | null;
+    front_default: string | null;
+    front_female: string | null;
+    front_shiny: string | null;
+    front_shiny_female: string | null;
+  };
+  stats: {
+    base_stat: number;
+    effort: number;
+    stat: {
+      name: string;
+      url: string;
+    };
+  }[];
+  types: {
+    slot: number;
+    type: {
+      name: string;
+      url: string;
+    };
+  }[];
+  weight: number;
+};
