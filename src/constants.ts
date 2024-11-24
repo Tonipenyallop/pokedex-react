cd021b7cdc8e543811d27f2b4aff2db70a708670
Tonipenyallop <kimtesu521@gmail.com>
2024-11-24 23:45:55 +0900

    add: constants

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export const GENERATIONS = [
+  { genId: "first", title: "FIRST" },
+  { genId: "second", title: "SECOND" },
+  { genId: "third", title: "THIRD" },
+  { genId: "fourth", title: "FOURTH" },
+];
