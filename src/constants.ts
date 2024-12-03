91c344e6a213e7776a70ba697339a007a1599a1a
Tonipenyallop <kimtesu521@gmail.com>
2024-12-03 19:41:56 +0900

    add: constants

--- src/constants.ts
+++ src/constants.ts
@@ -4,6 +4,10 @@ export const GENERATIONS = [
   { genId: "third", title: "THIRD" },
   { genId: "fourth", title: "FOURTH" },
   { genId: "fifth", title: "FIFTH" },
+  { genId: "sixth", title: "sixth" },
+  { genId: "seventh", title: "seventh" },
+  { genId: "eighth", title: "eighth" },
+  { genId: "ninth", title: "ninth" },
 ];
 
 export const BGM_MAX_NUM_IDX = 45;
