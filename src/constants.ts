661f0b7bb5cab7d80709fc93720d849cbf4d5188
Tonipenyallop <kimtesu521@gmail.com>
2024-12-04 19:32:47 +0900

    update: constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,13 +1,29 @@
 export const GENERATIONS = [
+  { genId: "all", title: "ALL" },
   { genId: "first", title: "FIRST" },
   { genId: "second", title: "SECOND" },
   { genId: "third", title: "THIRD" },
   { genId: "fourth", title: "FOURTH" },
   { genId: "fifth", title: "FIFTH" },
-  { genId: "sixth", title: "sixth" },
-  { genId: "seventh", title: "seventh" },
-  { genId: "eighth", title: "eighth" },
-  { genId: "ninth", title: "ninth" },
+  { genId: "sixth", title: "SIXTH" },
+  { genId: "seventh", title: "SEVENTH" },
+  { genId: "eighth", title: "EIGHTH" },
+  { genId: "ninth", title: "NINTH" },
 ];
 
 export const BGM_MAX_NUM_IDX = 45;
+
+export const GEN_GROUPS = {
+  all: [1, 1025],
+  first: [1, 151],
+  second: [152, 251],
+  third: [252, 386],
+  fourth: [387, 493],
+  fifth: [494, 649],
+  sixth: [650, 721],
+  seventh: [722, 809],
+  eighth: [810, 905],
+  ninth: [906, 1025],
+};
+
+export const GEN_ID_KEY = "gen-id-key";
