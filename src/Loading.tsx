0130073a6b7cc1708e30070f2a020db45b01f6a2
Tonipenyallop <kimtesu521@gmail.com>
2025-01-06 22:57:39 +0900

    add: pokeball

---
+++ src/Loading.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Pokeball from "./Pokeball";
+
+const Loading = () => {
+  return (
+    <div>
+      <Pokeball />
+      <p className="loading-text">Loading ...</p>
+    </div>
+  );
+};
+
+export default Loading;
